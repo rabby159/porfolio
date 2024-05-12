0d7cf87b6c196b2e746eb940faf12774f7a2cd33
Md Rabby <ablazerabbi159@gmail.com>
2024-05-12 23:13:42 +0600

    update porfolio

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Main = () => {
     return (
-        <div className="bg-black">
+        <div>
             <Outlet></Outlet>
         </div>
     );
